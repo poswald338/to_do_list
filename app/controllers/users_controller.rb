3441000efcb4edab8f1aedd4b7c39cf184381734
Patrick Oswald <poswald338@gmail.com>
2022-03-17 20:04:22 -0500

    add two custom scopes and user model and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :priority, presence: true, numericality: {less_than_or_equal_to: 3}
+
+  scope :completed, -> {where(:isComplete => true)}
+  scope :long_title, -> {where("LENGTH(title) > 20")}
 end
