96473fd703eb59754068128a1bc5a77766ae796a
Patrick Oswald <poswald338@gmail.com>
2022-03-31 13:00:14 -0500

    create session when user logs in/ add logout to navigation partial

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  
+  def new
+  
+  end
+  def create
+    user = User.find_by(email: user_params[:email].downcase)
+    if user && user.authenticate(user_params[:password])
+      session[:current_user_id] = user.id
+      flash[:notice] = "Successfully logged in!"
+      redirect_to '/tasks'
+    else
+      flash.now[:alert] = "Somethiung went wrong."
+      render :new
+    end
+  end
+  def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = "Successfully logged out"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:email, :password)
+  end
+end
