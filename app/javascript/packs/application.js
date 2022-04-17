d24a49465e99e25d6cf4b2901ccd0f1a563ea587
Patrick Oswald <poswald338@gmail.com>
2022-04-17 11:45:41 -0500

    add font awesome icons and an icon to navigate to notifications from navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../controllers"
+import "@fortawesome/fontawesome-free/js/all";
 
 
 Rails.start()
